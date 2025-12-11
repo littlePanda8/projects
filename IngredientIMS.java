79c31ebdb00fef2a1ef5d1201fafc26f1499f4f3
ProjectX <144492715+unrealisticfaces@users.noreply.github.com>
2025-12-12 00:30:17 +0800

    Delete IngredientIMS.java

--- IngredientIMS.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-public class IngredientIMS {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new Login();    
-        });
-    }
-}
